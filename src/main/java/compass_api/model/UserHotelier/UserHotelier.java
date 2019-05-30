a99241bdb0e3acae5b4583443c59fb5ff91dedea
Emre Kaplan <emrekp@outlook.com>
2019-05-30 09:37:42 +0300

    CP-2606 - add needed fields and methods

--- src/main/java/compass_api/model/UserHotelier/UserHotelier.java
+++ src/main/java/compass_api/model/UserHotelier/UserHotelier.java
@@ -1,7 +1,16 @@
 package compass_api.model.UserHotelier;
 
 public class UserHotelier {
+	private ChannelManager channelManager;
 	private String channelManagerRateOption;
+
+	public ChannelManager getChannelManager() {
+		return channelManager;
+	}
+
+	public void setChannelManager(ChannelManager channelManager) {
+		this.channelManager = channelManager;
+	}
 
 	public String getChannelManagerRateOption() {
 		return channelManagerRateOption;
